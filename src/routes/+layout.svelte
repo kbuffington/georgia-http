06acc8578f378aa0885a306ddcbd191a32a5d50b
Kevin Buffington <kbuffington@gmail.com>
2023-02-05 17:59:48 -0600

    Initial layout mockup wish scss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<script>
+	import Header from './Header.svelte';
+</script>
+
+<div class="app">
+    <Header />
+
+    <main>
+        <slot />
+    </main>
+
+    <footer>
+        <p>Georgia-HTTP</p>
+    </footer>
+</div>
+
+<style lang="scss">
+    @import "../scss/colors.scss";
+
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        background: $bg-color;
+        font-family: 'Helvetica Neue LT Pro';
+        color: white;
+
+        main {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            padding: 1rem;
+            width: 100%;
+            max-width: 64rem;
+            margin: 0 auto;
+            box-sizing: border-box;
+        }
+
+        footer {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 12px;
+
+            a {
+                font-weight: bold;
+            }
+        }
+    }
+
+    @media (min-width: 480px) {
+        footer {
+            padding: 12px 0;
+        }
+    }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<header>
+    Header block
+</header>
+
+<style lang="scss">
+    @import "../scss/colors.scss";
+    $padding: 1rem;
+    $height: 100px; // guaranteed to be this height regardless of padding
+
+    header {
+        background-color: $menu-bg;
+        height: calc($height - 2 * $padding);
+        padding: $padding;
+    }
+</style>
