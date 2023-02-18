f330f4e7f40f86000ecd6b141a09c7e98ceafef0
kbuffington@gmail.com <kbuffington@gmail.com>
2023-02-17 23:56:32 -0600

    Adding @smui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,27 @@
 
 <svelte:head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Material Icons -->
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
     <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
     />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,100;1,300&display=swap" rel="stylesheet">
+    <!-- Roboto Mono -->
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto+Mono"
+    />
+
+    <!-- SMUI Styles -->
+    <link rel="stylesheet" href="/georgia/smui.css" media="(prefers-color-scheme: light)" />
+    <link rel="stylesheet" href="/georgia/smui-dark.css" media="screen and (prefers-color-scheme: dark)"
+    />
 </svelte:head>
 
 <div class="app">
