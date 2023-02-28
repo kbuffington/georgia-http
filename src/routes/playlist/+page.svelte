80f730ebe6a9b30cbd5a369f526afa674f8e83e1
kbuffington@gmail.com <kbuffington@gmail.com>
2023-02-27 19:27:01 -0600

    handle playlist item selection

--- src/routes/playlist/+page.svelte
+++ src/routes/playlist/+page.svelte
@@ -6,6 +6,13 @@
     import Textfield from '@smui/textfield';
     import Icon from '@smui/textfield/icon';
     import MiniArtwork from '../MiniArtwork.svelte';
+    import type { PlTrackData } from '@stores/types';
+
+    let selection: number[] = [];
+    let anchor: number = -1;
+
+    const forceUpdate = async (_: any) => {};
+    let doRerender = 0;
 
     function doSearch() {
         if ($searchString.length) {
@@ -18,6 +25,46 @@
      */
     function playItem(index: number) {
         playPlaylistItem(index);
+    }
+
+    function selectItem(evt: MouseEvent, index: number, item: PlTrackData) {
+        console.log(evt);
+        if (evt.ctrlKey) {
+            const found = selection.indexOf(item.plIndex);
+            if (found === -1) {
+                item = Object.assign(item, { selected: true });
+                selection.push(item.plIndex);
+            } else {
+                item = Object.assign(item, { selected: false });
+                selection.splice(found, 1); // remove
+            }
+            anchor = index;
+        } else if (evt.shiftKey && anchor !== -1) {
+            const start = Math.min(anchor, index);
+            const end = Math.max(anchor, index);
+
+            selection = $playlistData.tracks
+                .filter((_, index) => index >= start && index <= end)
+                .map(item => item.plIndex);
+        } else {
+            selection = [item.plIndex];
+            anchor = index;
+        }
+        // console.log(anchor, selection);
+    }
+
+    function rightClickContextMenu(evt: MouseEvent, index: number, item: PlTrackData) {
+        if (evt.ctrlKey && evt.buttons === 1) {
+            selectItem(evt, index, item);
+            doRerender++;
+        } else {
+            // show context menu?
+            console.log(evt);
+        }
+    }
+
+    function testClick(evt: MouseEvent) {
+        console.log('test!', evt);
     }
 </script>
 
@@ -34,47 +81,54 @@
             <div class="playlist-header">
                 <PlaylistHeader />
             </div>
-            <div class="playlist-data">
-                {#each $playlistData.tracks as item, i}
-                    <div
-                        class="item-row"
-                        class:active={item.active}
-                        class:focused={item.focused}
-                        class:newalbum={i > 0 &&
-                            (item.albumArtist !== $playlistData.tracks[i - 1].albumArtist ||
-                                item.album !== $playlistData.tracks[i - 1].album)}
-                        on:dblclick={() => playItem(item.plIndex)}
-                    >
-                        <div class="cell tracknum">{item.tracknumber}.</div>
-                        <div class="cell title">
-                            {#if i === 0 || item.albumArtist !== $playlistData.tracks[i - 1].albumArtist || item.album !== $playlistData.tracks[i - 1].album}
-                                {item.title}
-                                <span
-                                    class="dimmed"
-                                    title="{item.artist} / {item.date} / {item.album}"
+            {#await forceUpdate(doRerender) then _}
+                <div class="playlist-data">
+                    {#each $playlistData.tracks as item, i}
+                        <div
+                            class="item-row"
+                            class:active={item.active}
+                            class:focused={item.focused}
+                            class:selected={selection.includes(item.plIndex)}
+                            class:newalbum={i > 0 &&
+                                (item.albumArtist !== $playlistData.tracks[i - 1].albumArtist ||
+                                    item.album !== $playlistData.tracks[i - 1].album)}
+                            on:keydown={() => {}}
+                            on:contextmenu|preventDefault={evt =>
+                                rightClickContextMenu(evt, i, item)}
+                            on:click={evt => selectItem(evt, i, item)}
+                            on:dblclick={() => playItem(item.plIndex)}
+                        >
+                            <div class="cell tracknum">{item.tracknumber}.</div>
+                            <div class="cell title">
+                                {#if i === 0 || item.albumArtist !== $playlistData.tracks[i - 1].albumArtist || item.album !== $playlistData.tracks[i - 1].album}
+                                    {item.title}
+                                    <span
+                                        class="dimmed"
+                                        title="{item.artist} / {item.date} / {item.album}"
+                                    >
+                                        / {item.artist} / {item.date} / {item.album}</span
+                                    >
+                                {:else}
+                                    {item.title}
+                                {/if}
+                            </div>
+                            <div class="cell playcount">
+                                {#if item.playcount > 0}
+                                    {item.playcount}
+                                {/if}
+                            </div>
+                            <div class="cell rating">
+                                <span class="dimmed">| </span>
+                                <span class="star">{item.ratingStars}</span>
+                                <span class="empty dimmed" class:nostars={!item.ratingStars}
+                                    >{item.ratingEmpty}</span
                                 >
-                                    / {item.artist} / {item.date} / {item.album}</span
-                                >
-                            {:else}
-                                {item.title}
-                            {/if}
+                            </div>
+                            <div class="cell length">{item.displayLength}</div>
                         </div>
-                        <div class="cell playcount">
-                            {#if item.playcount > 0}
-                                {item.playcount}
-                            {/if}
-                        </div>
-                        <div class="cell rating">
-                            <span class="dimmed">| </span>
-                            <span class="star">{item.ratingStars}</span>
-                            <span class="empty dimmed" class:nostars={!item.ratingStars}
-                                >{item.ratingEmpty}</span
-                            >
-                        </div>
-                        <div class="cell length">{item.displayLength}</div>
-                    </div>
-                {/each}
-            </div>
+                    {/each}
+                </div>
+            {/await}
             <div class="playlist-footer">
                 <div class="playlist-footer-content">
                     <div class="item-row">
@@ -166,6 +220,7 @@
                 }
 
                 .item-row {
+                    color: rgb(230, 230, 230);
                     font-size: 12px;
                     display: table-row;
                     max-height: $pl-row-height;
@@ -179,15 +234,23 @@
                         background-color: var(--color) !important;
                     }
 
-                    &.focused {
-                        box-shadow: inset 0px 0px 0px 1px grey;
-                    }
-                    &:not(.focused).newalbum .cell {
+                    &:not(.selected).newalbum .cell {
                         // border-top: 1px solid var(--color);
                         border-top: 1px solid rgb(220, 220, 220);
                     }
                     &:not(.newalbum) .cell {
                         border-top: 1px solid rgba(0, 0, 0, 0); // need an invisible border so height doesn't get screwed up
+                    }
+
+                    &.selected {
+                        box-shadow: inset 0px 0px 0px 1px grey;
+
+                        .cell {
+                            color: white;
+                        }
+                        .cell.tracknum {
+                            border-left: 5px solid var(--color);
+                        }
                     }
 
                     div.cell {
@@ -197,6 +260,7 @@
                             font-weight: 500;
                             width: $pl-track-w;
                             text-align: right;
+                            box-sizing: border-box;
                         }
                         &.title {
                             font-weight: 300;
