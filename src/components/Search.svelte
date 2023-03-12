9fbaa35714dabf05e8ec687f4c62199b24e41780
kbuffington@gmail.com <kbuffington@gmail.com>
2023-03-12 13:13:26 -0500

    Change color of Search X button

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -34,7 +34,20 @@
             background: none;
             outline: none;
             border-radius: 0;
+            color: white;
             -webkit-appearance: none;
+
+            &::-webkit-search-cancel-button {
+                /* Remove default */
+                -webkit-appearance: none;
+                background-color: var(--primary);
+                height: 20px;
+                width: 20px;
+                -webkit-mask-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23777'><path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z'/></svg>");
+                background-size: 20px 20px;
+                left: 2px;
+                position: relative;
+            }
 
             &:focus {
                 & ~ .underline {
