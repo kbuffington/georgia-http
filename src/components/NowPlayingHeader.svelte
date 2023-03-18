4c83de5c14a22f46608be7e880c950f7a3a2b5aa
kbuffington@gmail.com <kbuffington@gmail.com>
2023-03-18 18:07:47 -0500

    Refactor NowPlayingHeader into component

---
+++ src/components/NowPlayingHeader.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import { trackInfo } from '@stores/fb-store';
+</script>
+
+<div class="artist-container">
+    <div class="artist">
+        {$trackInfo.artist}
+    </div>
+    {#each $trackInfo.artistCountryFlags as flag}
+        <img class="artist-flag" src="/georgia/images/flags/64/{flag}.png" alt="{flag} flag" />
+    {/each}
+</div>
+<div class="right-info">
+    <div class="year">{$trackInfo.year}</div>
+    <div class="codec">{$trackInfo.codecInfo}</div>
+</div>
+
+<style lang="scss">
+    @import '@css/constants.scss';
+
+    .artist-container {
+        .artist {
+            display: inline-block;
+            padding: 0 0.5rem 0.5rem 1rem;
+            font-size: 38px;
+            font-weight: 500;
+        }
+
+        .artist-flag {
+            display: inline-block;
+            height: 34px;
+            vertical-align: text-bottom;
+            padding-right: 0.25rem;
+        }
+    }
+
+    .right-info {
+        position: absolute;
+        right: 1rem;
+        top: calc($header-height + 0.5rem);
+
+        .year {
+            font-size: 42px;
+            font-weight: 700;
+            text-align: right;
+            margin-bottom: 0.25rem;
+        }
+
+        .codec {
+            font-weight: 100;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { fb, playingInfo, trackInfo } from '@stores/fb-store';
+    import { fb, playingInfo } from '@stores/fb-store';
     import Artwork from '@components/Artwork.svelte';
+    import NowPlayingHeader from '@components/NowPlayingHeader.svelte';
 </script>
 
 <div id="main-content">
@@ -9,22 +10,7 @@
             Loading...
         {:then}
             {#if !$fb.isStopped}
-                <div class="artist-container">
-                    <div class="artist">
-                        {$trackInfo.artist}
-                    </div>
-                    {#each $trackInfo.artistCountryFlags as flag}
-                        <img
-                            class="artist-flag"
-                            src="/georgia/images/flags/64/{flag}.png"
-                            alt="{flag} flag"
-                        />
-                    {/each}
-                </div>
-                <div class="right-info">
-                    <div class="year">{$trackInfo.year}</div>
-                    <div class="codec">{$trackInfo.codecInfo}</div>
-                </div>
+                <NowPlayingHeader />
                 <Artwork />
             {/if}
         {/await}
@@ -44,39 +30,6 @@
 
         .top-content {
             flex-grow: 1;
-
-            .artist-container {
-                .artist {
-                    display: inline-block;
-                    padding: 0 0.5rem 0.5rem 1rem;
-                    font-size: 38px;
-                    font-weight: 500;
-                }
-
-                .artist-flag {
-                    display: inline-block;
-                    height: 34px;
-                    vertical-align: text-bottom;
-                    padding-right: 0.25rem;
-                }
-            }
-
-            .right-info {
-                position: absolute;
-                right: 1rem;
-                top: calc($header-height + 0.5rem);
-
-                .year {
-                    font-size: 42px;
-                    font-weight: 700;
-                    text-align: right;
-                    margin-bottom: 0.25rem;
-                }
-
-                .codec {
-                    font-weight: 100;
-                }
-            }
         }
     }
 </style>
