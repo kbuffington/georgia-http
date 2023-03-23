5e82acdc439ecb364f2d53f53cb157e28955d6a7
kbuffington@gmail.com <kbuffington@gmail.com>
2023-03-23 12:42:51 -0500

    Increase width of search box

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -61,7 +61,7 @@
 
 <style lang="scss">
     .playlist-search {
-        z-index: 100; // above transport
+        z-index: 1002; // above transport
         position: absolute;
         left: 1rem;
         top: 2rem;
@@ -75,59 +75,63 @@
             cursor: pointer;
         }
 
-        input {
-            width: 100%;
-            height: 30px;
-            box-sizing: border-box;
-            line-height: 30px;
-            font-size: 14px;
-            border: 0;
-            background: none;
-            outline: none;
-            border-radius: 0;
-            color: white;
-            -webkit-appearance: none;
+        .input-box {
+            width: 12rem; // should this be a vw?
 
-            &::-webkit-search-cancel-button {
-                /* Remove default */
+            input {
+                width: 100%;
+                height: 30px;
+                box-sizing: border-box;
+                line-height: 30px;
+                font-size: 14px;
+                border: 0;
+                background: none;
+                outline: none;
+                border-radius: 0;
+                color: white;
                 -webkit-appearance: none;
-                background-color: var(--primary);
-                height: 20px;
-                width: 20px;
-                -webkit-mask-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23777'><path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z'/></svg>");
-                background-size: 20px 20px;
-                left: 2px;
-                position: relative;
-            }
 
-            &:focus {
-                & ~ .underline {
-                    width: 100%;
+                &::-webkit-search-cancel-button {
+                    /* Remove default */
+                    -webkit-appearance: none;
+                    background-color: var(--primary);
+                    height: 20px;
+                    width: 20px;
+                    -webkit-mask-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23777'><path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z'/></svg>");
+                    background-size: 20px 20px;
+                    left: 2px;
+                    position: relative;
                 }
-                & ~ .underline-not-focused {
-                    visibility: hidden;
+
+                &:focus {
+                    & ~ .underline {
+                        width: 100%;
+                    }
+                    & ~ .underline-not-focused {
+                        visibility: hidden;
+                    }
                 }
             }
-        }
-        .underline {
-            content: '';
-            display: block;
-            position: absolute;
-            bottom: -1px;
-            left: 0;
-            width: 0;
-            height: 2px;
-            background: var(--primary);
-            transition: all 200ms ease-out;
-        }
-        .underline-not-focused {
-            display: block;
-            position: absolute;
-            bottom: -1px;
-            left: 0;
-            width: 100%;
-            height: 2px;
-            border-bottom: 1px solid #ccc;
+            .underline {
+                content: '';
+                display: block;
+                position: absolute;
+                bottom: -1px;
+                left: 0;
+                width: 0;
+                height: 2px;
+                background: var(--primary);
+                transition: all 200ms ease-out;
+            }
+            .underline-not-focused {
+                display: block;
+                position: absolute;
+                bottom: -1px;
+                left: 0;
+                width: 100%;
+                height: 2px;
+                border-bottom: 1px solid #ccc;
+            }
         }
     }
 </style>
